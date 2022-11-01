dc3ea3cfae7af12bd3c4e4a043fbeef86c4727f1
avxman <wk.kheyder.a.v@gmail.com>
2022-11-01 17:59:03 +0200

    Версия Php >=8.* и laravel >=8.* исправлена ошибка при обновлении ветки из удалённого репозитория

--- src/Events/SiteEvent.php
+++ src/Events/SiteEvent.php
@@ -37,12 +37,6 @@ class SiteEvent extends BaseEvent
      * @throws \ErrorException
      */
     protected function pull(array $data) : void{
-        $branchTest = 'test';
-        $branch = str_replace('On branch ', '', stristr($this->commandGenerate("status"), PHP_EOL, true));
-        $this->commandGenerate("checkout -b {$branchTest}");
-        $this->commandGenerate("branch -D {$branch}");
-        $this->commandGenerate("checkout {$branch}");
-        $this->commandGenerate("branch -D {$branchTest}");
         $command = $this->commandGenerate("pull");
         if(Str::contains(Str::lower($command), 'error')){
             $comm = $this->commandGenerate("stash save --keep-index");
@@ -55,12 +49,18 @@ class SiteEvent extends BaseEvent
                 $command = $comm;
             }
         }
+        $branchTest = 'test';
+        $branch = str_replace('On branch ', '', stristr($this->commandGenerate("status"), PHP_EOL, true));
+        $reload = PHP_EOL.$this->commandGenerate("checkout -b {$branchTest}");
+        $reload .= PHP_EOL.$this->commandGenerate("branch -D {$branch}");
+        $reload .= PHP_EOL.$this->commandGenerate("checkout {$branch}");
+        $reload .= PHP_EOL.$this->commandGenerate("branch -D {$branchTest}");
         $this->writtingLog(
             'SiteEvent: %1, result: %2',
             ['%1', '%2'],
-            ['pull', $command]
+            ['pull', $command.$reload]
         );
-        $this->result = [$command];
+        $this->result = [$command, $reload];
     }
 
     /**
